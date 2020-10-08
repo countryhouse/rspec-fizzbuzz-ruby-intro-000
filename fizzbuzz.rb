b928c2655fbcc6edeac237ad922f741aba62ba5c
May Geolot <meriel.may@gmail.com>
2020-10-08 09:34:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(num)
+  if num % 3 == 0
     "Fizz"
-  elsif int % 5 == 0
+  elsif num % 5 == 0
     "Buzz"
-  elsif int % 15 == 0 
+  elsif num % 15 == 0 
     "FizzBuzz"
   else
     nil
